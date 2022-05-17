dfd11ceb9b915a538452f02cfaa4e5c0fb592b05
JadKHaddad <jadkhaddad@gmail.com>
2022-05-17 23:57:40 +0200

    cursor

--- src/lib.rs
+++ src/lib.rs
@@ -8,27 +8,29 @@ use crossterm::style::{Color, Print, ResetColor, SetBackgroundColor, SetForegrou
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
 use std::io::stdout;
 
-pub struct Menu<'a, F> where
-F: Fn(&str) -> String
-{
+pub struct Menu<'a> {
     title: &'a str,
     options: &'a Vec<&'a str>,
     selected_index: usize,
     stdout: std::io::Stdout,
-    title_offset: usize,
+    title_vertical_offset: usize,
+    title_horizontal_offset: usize,
     new_line_count: usize,
-    option_formater: F,
+    selector: &'a str,
+    selector_offset_left: usize,
+    selector_offset_right: usize,
 }
 
-impl<'a, F> Menu<'a, F> where
-F:Fn(&str) -> String
-{
+impl<'a> Menu<'a> {
     pub fn new(
         title: &'a str,
         options: &'a Vec<&'a str>,
-        title_offset: usize,
+        title_vertical_offset: usize,
+        title_horizontal_offset: usize,
         selected_index: usize,
-        option_formater: F,
+        selector: &'a str,
+        selector_offset_left: usize,
+        selector_offset_right: usize,
     ) -> Result<Self, String> {
         if selected_index >= options.len() {
             return Err(format!(
@@ -38,11 +40,13 @@ F:Fn(&str) -> String
         }
 
         let console_size = termsize::get().unwrap();
-        if title.len() as u16 > console_size.cols {
+        if title.len() + title_horizontal_offset > console_size.cols as usize {
             return Err(String::from("Title is too long"));
         }
         for (i, option) in options.iter().enumerate() {
-            if option.len() as u16 > console_size.cols {
+            if option.len() + selector.len() + selector_offset_left + selector_offset_right
+                > console_size.cols as usize
+            {
                 return Err(format!("Option [{}] is too long", i));
             }
             if option.contains('\n') {
@@ -53,7 +57,7 @@ F:Fn(&str) -> String
             }
         }
         let new_line_count = title.matches('\n').count();
-        if 2 + new_line_count + title_offset + options.len() > console_size.rows as usize {
+        if 2 + new_line_count + title_vertical_offset + options.len() > console_size.rows as usize {
             return Err(String::from("Menu will not fit on screen"));
         }
         Ok(Menu {
@@ -61,19 +65,22 @@ F:Fn(&str) -> String
             options,
             selected_index: selected_index,
             stdout: stdout(),
-            title_offset,
+            title_vertical_offset,
+            title_horizontal_offset,
             new_line_count,
-            option_formater,
+            selector,
+            selector_offset_left,
+            selector_offset_right,
         })
     }
 
     fn format_option(&self, index: usize) -> String {
-        format!("{} - {}\n", index, (self.option_formater)(self.options[index]))
+        format!("{}\n", self.options[index])
     }
 
     fn format_title(&self) -> String {
         let mut title = format!("{}\n", self.title);
-        for _ in 0..self.title_offset {
+        for _ in 0..self.title_vertical_offset {
             title.push('\n');
         }
         title
@@ -84,7 +91,7 @@ F:Fn(&str) -> String
         execute!(
             self.stdout,
             Clear(ClearType::All),
-            cursor::MoveTo(0, 0),
+            cursor::MoveTo(self.title_horizontal_offset as u16, 0),
             Print(out)
         )
         .unwrap();
@@ -93,14 +100,26 @@ F:Fn(&str) -> String
             if i == self.selected_index {
                 execute!(
                     self.stdout,
+                    cursor::MoveRight(self.selector_offset_left as u16),
                     SetForegroundColor(Color::Blue),
+                    Print(self.selector),
+                    cursor::MoveRight(self.selector_offset_right as u16),
                     Print(out),
                     ResetColor
                 )
                 .unwrap();
                 continue;
             }
-            execute!(self.stdout, Print(out)).unwrap();
+            execute!(
+                self.stdout,
+                cursor::MoveRight(
+                    self.selector_offset_left as u16
+                        + self.selector_offset_right as u16
+                        + self.selector.len() as u16
+                ),
+                Print(out)
+            )
+            .unwrap();
         }
         execute!(
             self.stdout,
@@ -108,7 +127,7 @@ F:Fn(&str) -> String
                 0,
                 self.selected_index as u16
                     + self.new_line_count as u16
-                    + self.title_offset as u16
+                    + self.title_vertical_offset as u16
                     + 1
             )
         )
@@ -132,10 +151,18 @@ F:Fn(&str) -> String
                         execute!(
                             self.stdout,
                             Clear(ClearType::CurrentLine),
+                            cursor::MoveRight(
+                                self.selector_offset_left as u16
+                                    + self.selector_offset_right as u16
+                                    + self.selector.len() as u16
+                            ),
                             Print(current_line_out),
                             cursor::MoveToPreviousLine(2),
                             Clear(ClearType::CurrentLine),
+                            cursor::MoveRight(self.selector_offset_left as u16),
                             SetForegroundColor(Color::Blue),
+                            Print(self.selector),
+                            cursor::MoveRight(self.selector_offset_right as u16),
                             Print(next_line_out),
                             ResetColor,
                             cursor::MoveToPreviousLine(1)
@@ -154,9 +181,17 @@ F:Fn(&str) -> String
                         execute!(
                             self.stdout,
                             Clear(ClearType::CurrentLine),
+                            cursor::MoveRight(
+                                self.selector_offset_left as u16
+                                    + self.selector_offset_right as u16
+                                    + self.selector.len() as u16
+                            ),
                             Print(current_line_out),
                             Clear(ClearType::CurrentLine),
+                            cursor::MoveRight(self.selector_offset_left as u16),
                             SetForegroundColor(Color::Blue),
+                            Print(self.selector),
+                            cursor::MoveRight(self.selector_offset_right as u16),
                             Print(next_line_out),
                             ResetColor,
                             cursor::MoveToPreviousLine(1)

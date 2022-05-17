c7616bc172531c11d71639e7aba2852dacf682dd
JadKHaddad <jadkhaddad@gmail.com>
2022-05-18 00:07:35 +0200

    return type set

--- src/lib.rs
+++ src/lib.rs
@@ -7,30 +7,23 @@ use crossterm::event::{read, Event, KeyCode, KeyEvent, KeyModifiers};
 use crossterm::style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
 use std::io::stdout;
+use std::process;
 
 pub struct Menu<'a> {
     title: &'a str,
     options: &'a Vec<&'a str>,
     selected_index: usize,
     stdout: std::io::Stdout,
-    title_vertical_offset: usize,
-    title_horizontal_offset: usize,
     new_line_count: usize,
     selector: &'a str,
-    selector_offset_left: usize,
-    selector_offset_right: usize,
 }
 
 impl<'a> Menu<'a> {
     pub fn new(
         title: &'a str,
         options: &'a Vec<&'a str>,
-        title_vertical_offset: usize,
-        title_horizontal_offset: usize,
         selected_index: usize,
         selector: &'a str,
-        selector_offset_left: usize,
-        selector_offset_right: usize,
     ) -> Result<Self, String> {
         if selected_index >= options.len() {
             return Err(format!(
@@ -40,13 +33,11 @@ impl<'a> Menu<'a> {
         }
 
         let console_size = termsize::get().unwrap();
-        if title.len() + title_horizontal_offset > console_size.cols as usize {
+        if title.len() > console_size.cols as usize {
             return Err(String::from("Title is too long"));
         }
         for (i, option) in options.iter().enumerate() {
-            if option.len() + selector.len() + selector_offset_left + selector_offset_right
-                > console_size.cols as usize
-            {
+            if option.len() + selector.len() > console_size.cols as usize {
                 return Err(format!("Option [{}] is too long", i));
             }
             if option.contains('\n') {
@@ -57,7 +48,7 @@ impl<'a> Menu<'a> {
             }
         }
         let new_line_count = title.matches('\n').count();
-        if 2 + new_line_count + title_vertical_offset + options.len() > console_size.rows as usize {
+        if 2 + new_line_count + options.len() > console_size.rows as usize {
             return Err(String::from("Menu will not fit on screen"));
         }
         Ok(Menu {
@@ -65,12 +56,8 @@ impl<'a> Menu<'a> {
             options,
             selected_index: selected_index,
             stdout: stdout(),
-            title_vertical_offset,
-            title_horizontal_offset,
             new_line_count,
             selector,
-            selector_offset_left,
-            selector_offset_right,
         })
     }
 
@@ -79,11 +66,7 @@ impl<'a> Menu<'a> {
     }
 
     fn format_title(&self) -> String {
-        let mut title = format!("{}\n", self.title);
-        for _ in 0..self.title_vertical_offset {
-            title.push('\n');
-        }
-        title
+        format!("{}\n", self.title)
     }
 
     pub fn display(&mut self) {
@@ -91,7 +74,7 @@ impl<'a> Menu<'a> {
         execute!(
             self.stdout,
             Clear(ClearType::All),
-            cursor::MoveTo(self.title_horizontal_offset as u16, 0),
+            cursor::MoveTo(0, 0),
             Print(out)
         )
         .unwrap();
@@ -100,10 +83,8 @@ impl<'a> Menu<'a> {
             if i == self.selected_index {
                 execute!(
                     self.stdout,
-                    cursor::MoveRight(self.selector_offset_left as u16),
                     SetForegroundColor(Color::Blue),
                     Print(self.selector),
-                    cursor::MoveRight(self.selector_offset_right as u16),
                     Print(out),
                     ResetColor
                 )
@@ -112,11 +93,7 @@ impl<'a> Menu<'a> {
             }
             execute!(
                 self.stdout,
-                cursor::MoveRight(
-                    self.selector_offset_left as u16
-                        + self.selector_offset_right as u16
-                        + self.selector.len() as u16
-                ),
+                cursor::MoveRight(self.selector.len() as u16),
                 Print(out)
             )
             .unwrap();
@@ -125,16 +102,22 @@ impl<'a> Menu<'a> {
             self.stdout,
             cursor::MoveTo(
                 0,
-                self.selected_index as u16
-                    + self.new_line_count as u16
-                    + self.title_vertical_offset as u16
-                    + 1
+                self.selected_index as u16 + self.new_line_count as u16 + 1
             )
         )
         .unwrap();
     }
 
-    pub fn run(&mut self) {
+    pub fn restore_console(&mut self) {
+        execute!(
+            self.stdout,
+            Clear(ClearType::All),
+            cursor::MoveTo(0, 0),
+            cursor::Show,
+        )
+        .unwrap();
+    }
+    pub fn run(&mut self) -> Option<usize> {
         enable_raw_mode().unwrap();
         execute!(self.stdout, cursor::Hide).unwrap();
         self.display();
@@ -151,18 +134,12 @@ impl<'a> Menu<'a> {
                         execute!(
                             self.stdout,
                             Clear(ClearType::CurrentLine),
-                            cursor::MoveRight(
-                                self.selector_offset_left as u16
-                                    + self.selector_offset_right as u16
-                                    + self.selector.len() as u16
-                            ),
+                            cursor::MoveRight(self.selector.len() as u16),
                             Print(current_line_out),
                             cursor::MoveToPreviousLine(2),
                             Clear(ClearType::CurrentLine),
-                            cursor::MoveRight(self.selector_offset_left as u16),
                             SetForegroundColor(Color::Blue),
                             Print(self.selector),
-                            cursor::MoveRight(self.selector_offset_right as u16),
                             Print(next_line_out),
                             ResetColor,
                             cursor::MoveToPreviousLine(1)
@@ -181,17 +158,11 @@ impl<'a> Menu<'a> {
                         execute!(
                             self.stdout,
                             Clear(ClearType::CurrentLine),
-                            cursor::MoveRight(
-                                self.selector_offset_left as u16
-                                    + self.selector_offset_right as u16
-                                    + self.selector.len() as u16
-                            ),
+                            cursor::MoveRight(self.selector.len() as u16),
                             Print(current_line_out),
                             Clear(ClearType::CurrentLine),
-                            cursor::MoveRight(self.selector_offset_left as u16),
                             SetForegroundColor(Color::Blue),
                             Print(self.selector),
-                            cursor::MoveRight(self.selector_offset_right as u16),
                             Print(next_line_out),
                             ResetColor,
                             cursor::MoveToPreviousLine(1)
@@ -200,23 +171,27 @@ impl<'a> Menu<'a> {
                     }
                 }
                 Event::Key(KeyEvent {
+                    code: KeyCode::Enter,
+                    modifiers: KeyModifiers::NONE,
+                }) => break,
+                Event::Key(KeyEvent {
                     code: KeyCode::Esc,
                     modifiers: KeyModifiers::NONE,
-                    //clearing the screen and printing our message
-                }) => break,
+                }) => {
+                    self.restore_console();
+                    return None;
+                }
                 Event::Key(KeyEvent {
                     code: KeyCode::Char('c'),
                     modifiers: KeyModifiers::CONTROL,
-                }) => break,
+                }) => {
+                    self.restore_console();
+                    process::exit(0);
+                }
                 _ => continue,
             }
         }
-        execute!(
-            self.stdout,
-            Clear(ClearType::All),
-            cursor::MoveTo(0, 0),
-            cursor::Show,
-        )
-        .unwrap();
+        self.restore_console();
+        Some(self.selected_index)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use menu::Menu;
 
 fn main() {
-    let options = vec!["Option 1", "Option 2", "Option 3", "Option 4", "Option 5", "Option 6"];
-    let mut menu = match Menu::new("Main Menu!", &options, 1, 0, 0, "       >      ", 0, 0) {
+    let options = vec![
+        "Option 1", "Option 2", "Option 3", "Option 4", "Option 5", "Option 6",
+    ];
+    let mut menu = match Menu::new("Main Menu!\n", &options, 0, "> ") {
         Ok(menu) => menu,
         Err(err) => {
             println!("{}", err);
             return;
         }
     };
-    menu.run();
+    println!("{:?}", menu.run());
 }
-
